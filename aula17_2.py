a971aca0cff39d444fb12bd68a658a222d07c952
willianbazarin <125612436+willianbazarin@users.noreply.github.com>
2023-02-18 09:48:42 -0400

    atualizaçao de aula

---
+++ aula17_2.py
@@ -0,0 +1,14 @@
+# if / elif / else
+# se / se não se / se não
+condição = False
+
+if condição:
+    print('este é o codigo do if')
+# para ser executado dentro da condição tem que dar o espaçamento do tab,
+# se for menor sera lido normalmente pelo interpretado. 
+else:
+    print('sera executado se a primeira condição não for satisfeita')
+
+
+if 10 == 10:
+    print('pode se ter mais de um if')
---
+++ aula17_3.py
@@ -0,0 +1,20 @@
+# if / elif / else
+# se / se não se / se não
+condição1 = False
+condição2 = False
+condição3 = True
+condição4 = False
+# ... ou pass é para nao escrever determinado codigo, o interpretador vai passa por ele,
+# e executara o proximo codigo. 
+
+if condição1:
+    print('codigo condição 1')
+elif condição2:
+    print('codigo condição 2')
+elif condição3:
+    print('codigo condição 3')
+elif condição4:
+    print('codigo condição 4')
+else:
+    print('nenhuma condição foi satisfeita')
+
